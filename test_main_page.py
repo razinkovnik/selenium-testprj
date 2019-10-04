9dbd3374c1a1767a380e1808aba71779972387fa
Nikita Razinkov <razinkovnik@gmail.com>
2019-10-04 17:25:16 +0300

    refactor test to class

--- test_main_page.py
+++ test_main_page.py
@@ -5,20 +5,20 @@ from pages.product_page import ProductPage
 import time
 
 @pytest.mark.skip
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-@pytest.mark.skip
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_add_product_to_basket()
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_add_product_to_basket()
 
 @pytest.mark.skip
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
--- test_product_page.py
+++ test_product_page.py
@@ -84,6 +84,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.skip
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     page = ProductPage(browser, link, timeout=1)
